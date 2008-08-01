5c840096821ab922a09b99ae3c0dffca3c8fce2c
Tomas Restrepo <tomas@winterdom.com>
2008-07-31 23:12:14 -0500

    Updated vim2css script: bug fixes and tweaked code

--- scripts/convert-vim2css.ps1
+++ scripts/convert-vim2css.ps1
@@ -1,42 +1,43 @@
 param( [string] $vimfile )
 
-(gc $vimfile) | %{
-   $nrx = "hi (?<n>\w+)"
-   $fgrx = "guifg=(?<n>#\w+)"
-   $bgrx = "guibg=(?<n>#\w+)"
-   $frx = "gui=(?<n>\w+)"
+# some instructions we don't care for
+$ignorable = ( 'link', 'clear' )
 
-   $isDef = $false
-   if ( $_ -match $nrx ) {
-      if ( $matches.n -eq 'Normal' ) {
-         $isDef = true;
-         write '.codebg {'
-         write '   border-left: solid 1em #303030;'
-         write '   font-size: 1.1em;'
-         write '   padding: 0.8em 0.5em;'
-      } else {
-         write ".$($matches.n) {"
-      }
-      if ( $_ -match $fgrx ) {
-         write "   color: $($matches.n);"
-      }
-      if ( $_ -match $bgrx ) {
-         write "   background: $($matches.n);"
-      }
-      if ( $_ -match $frx ) {
-         if ( $matches.n.indexOf("italic") -ge 0 ) {
-            write "   font-style: italic;"
-         }
-         if ( $matches.n.indexOf("bold") -ge 0 ) {
-            write "   font-weight: bold;"
-         }
-      }
-      write "}"
+$nrx = 'hi (?<n>\w+)'
+$fgrx = 'guifg=(?<n>#\w+)'
+$bgrx = 'guibg=(?<n>#\w+)'
+$frx = 'gui=(?<n>\S+)'
+
+(gc $vimfile) | ?{ 
+   ($_ -match $nrx) -and ($ignorable -notcontains $matches.n) 
+} | %{
+   if ( $matches.n -eq 'Normal' ) {
+      write '.codebg {'
+      write '   border-left: solid 1em #303030;'
+      write '   font-size: 1.1em;'
+      write '   padding: 0.8em 0.5em;'
+   } else {
+      write ".$($matches.n) {"
    }
+   if ( $_ -match $fgrx ) {
+      write "   color: $($matches.n);"
+   }
+   if ( $_ -match $bgrx ) {
+      write "   background: $($matches.n);"
+   }
+   # element could any combination of these
+   if ( $_ -match $frx ) {
+      switch ( $matches.n.split(',') ) {
+         "italic" { write "   font-style: $_;" }
+         "bold" { write "   font-weight: $_;" }
+         "underline" { write "   text-decoration: $_;" }
+      }
+   }
+   write '}'
 }
 
-# write the boilerplate part
-write "code {"
-write "   font-family: Consolas, `"DejaVu Sans Mono`", `"Lucida Console`", monospace; "
-write "}"
+# other boilerplate code
+write 'code {'
+write '   font-family: Consolas, "DejaVu Sans Mono", "Lucida Console", monospace; '
+write '}'
 
