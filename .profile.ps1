140bca4289027d6de3873619f023181feb7c37c9
Tomas Restrepo <tomas@winterdom.com>
2009-09-03 17:52:12 -0500

    Added helper ps functions for debugging

--- .profile.ps1
+++ .profile.ps1
@@ -164,6 +164,38 @@ function to-binle([long]$val) {
    [Convert]::ToString($val, 2)
 }
 
+function byteToChar([byte]$b) {
+   if ( $b -lt 32 -or $b  -gt 127 ) {
+      '.'
+   } else {
+      [char]$b
+   }
+}
+function format-bytes($bytes, $bytesPerLine = 8) {
+   $buffer = new-object system.text.stringbuilder
+   for ( $offset=0; $offset -lt $bytes.Length; $offset += $bytesPerLine ) {
+      [void]$buffer.AppendFormat('{0:X8}   ', $offset)
+      $numBytes = [math]::min($bytesPerLine, $bytes.Length - $offset)
+      for ( $i=0; $i -lt $numBytes; $i++ ) {
+         [void]$buffer.AppendFormat('{0:X2} ', $bytes[$offset+$i])
+      }
+      [void]$buffer.Append(' ' *((($bytesPerLine - $numBytes)*3)+3))
+      for ( $i=0; $i -lt $numBytes; $i++ ) {
+         [void]$buffer.Append( (byteToChar $bytes[$offset + $i]) )
+      }
+      [void]$buffer.Append("`n")
+   }
+   $buffer.ToString()
+}
+function convertfrom-b64([string] $str) {
+   [convert]::FromBase64String($str)
+}
+function normalize-array($array, [int]$offset, [int]$len) {
+   $dest = new-object $array.GetType() $len
+   [array]::Copy($array, $offset, $dest, 0, $len)
+   $dest
+}
+
 # load session helpers
 ."$SCRIPTS\sessions.ps1"
 
